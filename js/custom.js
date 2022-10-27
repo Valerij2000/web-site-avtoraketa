2f998c139a13cd45b7bf56bab2ab6c58500e0567
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-10-27 15:16:14 +0400

    added watsapp chat

--- js/custom.js
+++ js/custom.js
@@ -516,27 +516,6 @@ Assigned to: ThemeForest
 
 
     });
-
-    // Window Scroll
-    $(window).scroll(function() {
-        var wh = window.innerWidth;
-        //Go to top
-        if ($(this).scrollTop() > 100) {
-            $('.gotop').addClass('goto');
-        } else {
-            $('.gotop').removeClass('goto');
-        }
-
-    });
-    $(".gotop").on("click", function() {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
-        return false
-    });
-
-
-
 })(jQuery);
             if ($('.impl_change_text').length > 0) {
                 var $slogans = $("p.slogan").find("strong");
