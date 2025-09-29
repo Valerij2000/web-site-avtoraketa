6f300452039c3242edabadd2890599e9b416f9a0
Валерий <valery.shumkov@mail.ru>
2025-09-29 16:08:10 +0300

    design updates

--- src/scripts/modules/initial-popups.js
+++ src/scripts/modules/initial-popups.js
@@ -4,18 +4,10 @@ import 'bootstrap';
 const sec1 = 60;
 const sec2 = 10;
 
-const popupKorea = () => {
+const autoPopupInit = () => {
   setTimeout(() => {
-    $('#modal-auto-korea').modal('show');
-  }, sec1*1000);
-}
-
-const popupCredit = () => {
-  setTimeout(() => {
-    $('#modal-credit').modal('show');
+    $('#info').modal('show');
   }, sec2*1000);
 }
 
-// popupCredit();
-
-// popupKorea();
+autoPopupInit();
